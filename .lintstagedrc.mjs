f8b027856f36b43f9fb681c273fbdae3b328352f
Tiago Porto <tiago@tiagoporto.com>
2025-11-23 20:45:35 -0300

    chore: set ts

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -2,7 +2,7 @@ export default {
   '*': 'prettier --check --ignore-unknown --write',
   '*.{md,markdown,mdx}': ['remark --frail', 'eslint --max-warnings 0'],
   '*.{css,scss}': 'stylelint --fix',
-  // '*.{ts,tsx}': () => 'tsc --project tsconfig.json',
+  '*.{ts,tsx}': () => 'tsc --project tsconfig.json',
   '*.{js,mjs,jsx,ts,tsx}': [
     'eslint --max-warnings 0',
     //   'jest --bail --findRelatedTests --passWithNoTests',
